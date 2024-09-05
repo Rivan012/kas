07a2c5d4488814146dff444a10efcb12270e0001
Rivan012 <114328078+Rivan012@users.noreply.github.com>
2024-09-05 09:58:23 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,1 +1,14 @@
+<?php 
+header("Content-Type: application/json");
 
+if($_SERVER['REQUEST_METHOD'] === 'GET') {
+    $request = array(
+        'message' => 'Hello World!'
+        
+    );
+    echo json_encode($request);
+}else{
+    http_response_code(405);
+    echo json_encode(array('message' => 'Method Not Allowed'));
+}
+?>
